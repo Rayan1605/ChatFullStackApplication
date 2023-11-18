50a9e175776ee48a92605650e3314fd65703d412
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-11-18 04:53:06 +0000

    Start

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,6 +1,22 @@
+// Application - Main Express app object
+// json, urlencoded - Parse request body data
+// Response - HTTP response sent to client
+// Request - HTTP request received from client
+// NextFunction - Pass to next middleware function
+
+//on - This reads and understands JSON data from request bodies.
+// urlencoded - This reads and understands URL-encoded data from request bodies.
+
+
+//The ChattyServer class configures and starts an Express application.
+// It sets up middleware functions for security, standard features, routes, and global error handling.
+// It starts an HTTP server and Socket.IO for realtime communication.
+
+//Middleware are functions that run between receiving a request and sending a response
 import { Application, json, urlencoded, Response, Request, NextFunction}  from  'express';
 import * as http from "http";
 export class ChattyServer {
+    //The constructor takes in an Express Application object
    private app: Application;
        constructor(app: Application) {
            this.app = app;
@@ -13,6 +29,22 @@ export class ChattyServer {
            this.globalErrorHandler(this.app);
            this.startServer(this.app);
        }
+
+       //The start method configures the middlewares and starts the server.
+
+    // securityMiddlewares sets up security related middleware.
+
+    // standardMiddlewares sets up middleware for standard app features.
+
+    // routeMiddlewares sets up middleware for API routes.
+
+    // globalErrorHandler handles errors from all parts of the app.
+
+    // startServer starts the HTTP server using the Express app.
+
+    // createSocketIO initializes Socket.IO for realtime communication.
+
+    // startHttpServer starts listening on a port for HTTP requests.
 
           private securityMiddlewares(app:Application): void {}
 
