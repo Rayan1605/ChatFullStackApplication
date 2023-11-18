7188379f3d18dceb79efc6cdcaf8829034b7d789
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-11-18 05:36:28 +0000

    Start

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, {Express} from "express";
 
 import {ChattyServer} from "./setupServer";
 
+
+
 class Application {
     public initialize(): void {
         const app = express(); // create express app
