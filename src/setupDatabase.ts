a5863dd7ae3f69128aee8335fa62c1d8b3b563be
rayan <rayanzyad99@gmail.com>
2024-02-03 03:32:44 +0000

    Fixing DataConnection

--- src/setupDatabase.ts
+++ src/setupDatabase.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
-export default () => { //
+//export is like saying I have to share my robot
+// default is basically saying this is my special robot and if you ask me for one with telling me which one then you get this one
+
+export default () => {
 
     const connect = () => {
         //Returning a promise so we will use then and catch
