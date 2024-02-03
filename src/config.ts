6fbfad3d80f2f93b13f9f4313ac3416084d31ec2
rayan <rayanzyad99@gmail.com>
2024-02-03 14:42:52 +0000

    Fixing DataConnection

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,15 @@
 //These are used to organize and manage configuration settings for application
+//These can include things like database connection or API keys
+//It good to have this because we can change, define and access these setting throughout our application
+
+
+
+
+
+
+
+
+
+
+
 
