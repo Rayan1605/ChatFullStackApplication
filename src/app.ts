7ac971f1e41fac95d42171d28074e4712dae5b59
rayan <rayanzyad99@gmail.com>
2024-02-03 03:43:09 +0000

    Fixing DataConnection

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import {ChattyServer} from "./setupServer";
 
 class Application {
     public initialize(): void {
+        //The Express = express is the make sure that the app only holds an express application
         const app: Express = express(); // create express app
         const server:ChattyServer = new ChattyServer(app); // create server
         server.start();// start server
