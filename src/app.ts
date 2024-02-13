596ee68edc80eb452376468cef115334817707fd
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2024-02-13 23:54:43 +0000

    Start

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,17 @@
 import express, {Express} from "express";
+
 import {ChattyServer} from "./setupServer";
-import databaseConnection from "./setupDatabase"
-import {config} from "./config";
+
+
 
 class Application {
-        public initialize(): void {
-            this.loadConfig();
-            databaseConnection();
-            //The Express = express is the make sure that the app only holds an express application
-            const app: Express = express(); // create express app
-
-            const server:ChattyServer = new ChattyServer(app); // create server
-            server.start();// start server
-        }
-
-        private loadConfig(): void {
-            // load configuration settings
-               config.validateConfig();
-               config.cloudinaryConfig();
-        }
-
+    public initialize(): void {
+        const app = express(); // create express app
+        const server:ChattyServer = new ChattyServer(app); // create server
+        server.start();// start server
+    }
 
 }
 
-
 const application:Application = new Application();
 application.initialize(); // initialize application
-
-
-// Setting up the cloud area because we are going to be adding or uploading files to the cloud
-// cloud ordinary is just a cloud platform that is used to store files and manipulate them
-
-
