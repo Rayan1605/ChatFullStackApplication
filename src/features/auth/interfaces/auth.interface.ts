dd8100b3af82a16894d5e377c6e7f9dbf7ba386f
rayan <rayanzyad99@gmail.com>
2024-02-19 23:51:30 +0000

    Refactoring

---
+++ src/features/auth/interfaces/auth.interface.ts
@@ -0,0 +1,35 @@
+import { Document} from "mongoose";
+import { ObjectId} from "mongodb";
+
+ declare global {
+     namespace Express {
+         interface Request {
+             currentUser?: AuthPayload;
+         }
+     }
+ }
+
+ export interface AuthPayload {
+
+   userId: string;
+   uId: string;
+    email: string;
+    username: string;
+    avatarColor: string
+     iat: number;
+ }
+
+ export interface IAuthDocument extends Document {
+     _id: string | ObjectId;
+     uId: string;
+     username: string;
+     email: string;
+     password?: string;
+     avatarColor: string;
+     createdAt: Date;
+     comparePassword(password: string): Promise<boolean>;
+
+
+
+
+ }
