da2aeab9bba8e842515ce2327d14f74658d374ab
rayan <rayanzyad99@gmail.com>
2024-02-21 08:59:36 +0000

    Refactoring

--- src/features/auth/models/auth.schema.ts
+++ src/features/auth/models/auth.schema.ts
@@ -23,20 +23,23 @@ const SALT_ROUND = 10;
         }
 )
 
+// Before saving the password, we hash it and that what the code is doing below
+
 authSchema.pre("save", async function (this: IAuthDocument, next: () => void) {
     const hashedPassword: string = await hash(this.password as string, SALT_ROUND);
     this.password = hashedPassword;
     next();
 })
 
+// This is a method that is used to compare the password that is being passed in with the password that is stored in the database
 authSchema.methods.comparePassword = async function ( password: string): Promise<boolean> {
     const hashedPassword: string = (this as unknown as IAuthDocument).password!;
     return compare(password, hashedPassword);
 }
-
+// This is a method that is used to hash the password that is being passed in
 authSchema.methods.hashPassword = async function (password: string): Promise<string> {
     return hash(password, SALT_ROUND);
 }
-
+// This is the model that is used to create the document
 const AuthModel: Model<IAuthDocument> = model<IAuthDocument>("Auth", authSchema, "auth");
  export { AuthModel }
