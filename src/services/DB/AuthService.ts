e476c23fe91586bec0a27fa8a9e731be60fd2699
rayan <rayanzyad99@gmail.com>
2024-02-21 09:30:48 +0000

    Refactoring

--- src/services/DB/AuthService.ts
+++ src/services/DB/AuthService.ts
@@ -1,8 +1,9 @@
+import {IAuthDocument} from "@root/features/auth/interfaces/auth.interface";
 
 class AuthService {
 
     // We want to see if the user or email exists in the database
-    public async getUserByUsernameOrEmail(username: string, email: string): Promise<any> {
-
+    public async getUserByUsernameOrEmail(username: string, email: string): Promise<IAuthDocument> {
+    const query = {}
     }
 }
