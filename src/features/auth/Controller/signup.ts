86c3eaae091985adfb79b1f6e5a3683a0278eeb8
rayan <rayanzyad99@gmail.com>
2024-02-21 10:24:15 +0000

    Refactoring

--- src/features/auth/Controller/signup.ts
+++ src/features/auth/Controller/signup.ts
@@ -2,9 +2,10 @@ import { ObjectId} from "mongodb";
 import {Request, Response} from "express";
 import {joiValidation} from "@root/globels/Joi-Validation/Validation";
 import { signupSchema } from "@root/features/auth/schemes/signup";
-import {IAuthDocument} from "@root/features/auth/interfaces/auth.interface";
+import {IAuthDocument, ISignUpData} from "@root/features/auth/interfaces/auth.interface";
 import {authService} from "@services/DB/AuthService";
 import {BadRequestError} from "@root/globels/error-handler";
+import {Helpers} from "@root/globels/helpers";
 
 export class Signup {
     //Joi validation is to ensure that data conforms to a specific structure and set of rules before
@@ -14,9 +15,27 @@ export class Signup {
      const { username, email, password, avatarColor, avatarImage} = req.body;
      const CheckifUserExist: IAuthDocument = await authService.getUserByUsernameOrEmail(username, email);
 
-     if (CheckifUserExist) {
-        throw new BadRequestError("User already exist");
-     }
+         if (CheckifUserExist) {
+             throw new BadRequestError("User already exist");
+         }
+
+         const authObjectId: ObjectId = new ObjectId();
+         const userObjectId: ObjectId = new ObjectId();
+         const uid = `${Helpers.generateRandomColor(12)}`
+
+    }
+
+    private signupData(data: ISignUpData): IAuthDocument {
+        const {_id, username, email, uId,password, avatarColor} = data;
+        return {
+            _id,
+            uId,
+            username: Helpers.firstletter(username),
+            email: Helpers.lowerEmail(email),
+            password,
+            avatarColor,
+            createdAt: new Date()
+        } as unknown as IAuthDocument;
 
     }
 }
