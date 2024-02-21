a9109471a61c8913b08128bca3e69eb8ab2a1660
rayan <rayanzyad99@gmail.com>
2024-02-21 09:29:59 +0000

    Refactoring

---
+++ src/services/DB/AuthService.ts
@@ -0,0 +1,8 @@
+
+class AuthService {
+
+    // We want to see if the user or email exists in the database
+    public async getUserByUsernameOrEmail(username: string, email: string): Promise<any> {
+
+    }
+}
