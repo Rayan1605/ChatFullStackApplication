26a1a19656c418f1c086f5de20fb3d737a83c462
rayan <rayanzyad99@gmail.com>
2024-02-21 09:22:13 +0000

    Refactoring

--- src/features/auth/Controller/signup.ts
+++ src/features/auth/Controller/signup.ts
@@ -1,7 +1,12 @@
 import { ObjectId} from "mongodb";
 import {Request, Response} from "express";
+import {joiValidation} from "@root/globels/Joi-Validation/Validation";
+import { signupSchema } from "@root/features/auth/schemes/signup";
 
 export class Signup {
+    //Joi validation is to ensure that data conforms to a specific structure and set of rules before
+    // it's processed by your application or stored in your database
+    @joiValidation(signupSchema)
     public async signup(req: Request, res: Response): Promise<void> {
         const {email, username, password, avatarColor} = req.body;
 
