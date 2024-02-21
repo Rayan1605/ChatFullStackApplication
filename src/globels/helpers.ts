8152a26e412afe671d0f99e55aef1b17f3bdace5
rayan <rayanzyad99@gmail.com>
2024-02-21 09:43:23 +0000

    Refactoring

---
+++ src/globels/helpers.ts
@@ -0,0 +1,11 @@
+export class Helpers {
+
+    static firstletter = (str: string): string => {
+     const Vstring = str.toLowerCase();
+        // the split For example, "hello world" becomes ["hello", "world"].
+        // then we map through each value and make the first letter capital and the rest of the letters lowercase
+
+        return Vstring.split(' ').map((value: string) => `${value.charAt(0).
+     toUpperCase()}${value.slice(1).toLowerCase()}`).join(' ')
+    }
+}
