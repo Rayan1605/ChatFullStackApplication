6e3f914d19ab231038f0654ee94da063e53cdd49
rayan <rayanzyad99@gmail.com>
2024-02-21 09:32:26 +0000

    Refactoring

--- src/services/DB/AuthService.ts
+++ src/services/DB/AuthService.ts
@@ -3,7 +3,10 @@ import {IAuthDocument} from "@root/features/auth/interfaces/auth.interface";
 class AuthService {
 
     // We want to see if the user or email exists in the database
+    //Redis only hold the user data so we will be making a request to the database to see if the user or email exists
     public async getUserByUsernameOrEmail(username: string, email: string): Promise<IAuthDocument> {
     const query = {}
+
+
     }
 }
