c1bd2a02ba77b63bfec25257cac4c0d87daad19a
rayan <rayanzyad99@gmail.com>
2024-03-01 01:46:13 +0000

    Refactoring

--- src/services/redis/user.cache.ts
+++ src/services/redis/user.cache.ts
@@ -73,7 +73,7 @@ export class UserCache extends BaseCache {
     const dataToSave: string [] = [...firstList, ...secondList, ...thirdList];
     // Time to create the actual method that will save the data to Redis
 
-        //Checking if there is an op
+        //Checking if there is an open connection to Redis
         try {
             if (!this.client.isOpen) {
                 await this.client.connect();
