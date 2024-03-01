196e7b85aa5dcd2693e895aec9859ae03253ef83
rayan <rayanzyad99@gmail.com>
2024-03-01 00:49:40 +0000

    Refactoring

---
+++ src/services/redis/user.cache.ts
@@ -0,0 +1,14 @@
+//Not every data we will save in the chache and the reason for that is that we want to save only the data
+// that we think the user wil frequently use. like for example post so we will save post data stored in the cache
+
+// In redis, we assess other users profile because we wasnt the data to be retrieved quickly and not wait
+
+// Only the data that we think the user will frequently use will be stored in the cache or the data that
+// we want to be retrieved quickly
+import {BaseCache} from "@services/redis/base.cache";
+
+export class userCache extends BaseCache {
+    constructor() {
+        super('userCache');
+    }
+}
