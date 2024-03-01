636463cd2e28bf260a6f04c7e3df0ff280c543a5
rayan <rayanzyad99@gmail.com>
2024-03-01 00:59:33 +0000

    Refactoring

--- src/services/redis/user.cache.ts
+++ src/services/redis/user.cache.ts
@@ -5,10 +5,17 @@
 
 // Only the data that we think the user will frequently use will be stored in the cache or the data that
 // we want to be retrieved quickly
+
+// You can save the data in lists, that is in arrays, you can save data sets or in objects, we're going to use sets
 import {BaseCache} from "@services/redis/base.cache";
+import {IUserDocument} from "@root/features/user/models/user.interface";
 
 export class UserCache extends BaseCache {
     constructor() {
         super('userCache');
+    }
+// key and userId helping to identify and createdUser providing the actual data to be saved.
+    public async saveUserToCache(key: string, userId: string, createdUser: IUserDocument):Promise<void>{
+
     }
 }
