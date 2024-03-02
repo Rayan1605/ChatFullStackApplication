a4941b28e74afa6c63251b4204c2586e2d245b8b
rayan <rayanzyad99@gmail.com>
2024-03-02 04:51:23 +0000

    Refactoring

--- src/services/queues/base.queus.ts
+++ src/services/queues/base.queus.ts
@@ -3,3 +3,22 @@ import Logger from "bunyan";
 import { createBullBoard} from '@bull-board/api';
 import { BullAdapter } from '@bull-board/api/bullAdapter';
 import { ExpressAdapter } from '@bull-board/express';
+import {config} from "@root/config";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
