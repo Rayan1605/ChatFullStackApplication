3b8f6463e81bea659d42e8dc44001dc1e4adc6a2
rayan <rayanzyad99@gmail.com>
2024-03-02 04:56:53 +0000

    Refactoring

--- src/services/queues/base.queus.ts
+++ src/services/queues/base.queus.ts
@@ -5,6 +5,24 @@ import { BullAdapter } from '@bull-board/api/bullAdapter';
 import { ExpressAdapter } from '@bull-board/express';
 import {config} from "@root/config";
 
+let bullAdapters: BullAdapter[] = [];
+
+export let serverAdapter: ExpressAdapter;
+
+export abstract class BaseQueue {
+    queue: Queue.Queue
+    log: Logger;
+
+    constructor(QueueName: string) {
+        this.queue = new Queue(QueueName, `${config.REDIS_HOST}`);
+        bullAdapters.push(new BullAdapter(this.queue));
+        bullAdapters = [...new Set(bullAdapters)];
+        serverAdapter = new ExpressAdapter();
+        serverAdapter.setBasePath('/queues');
+
+    }
+
+}
 
 
 
