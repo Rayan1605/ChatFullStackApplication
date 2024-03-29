ad8c73948de9a8cee83e4be7b2a8fd61b18a5cc6
rayan <rayanzyad99@gmail.com>
2024-03-29 15:13:53 +0000

    Refactoring

--- src/services/queues/auth.queue.ts
+++ src/services/queues/auth.queue.ts
@@ -3,12 +3,14 @@
 
 import { BaseQueue} from "@services/queues/base.queus";
 import {IAuthJob} from "src/features/auth/interfaces/auth.interface"
+import {authWorker} from "@worker/auth.worker";
 class AuthQueue extends BaseQueue {
     //  so auth is the name which is used to identify and manage a specific set of task within Redis
     // Then we add it to the Bull Adapter which is used to manage the queue and we can use it to create a dashboard
 
     constructor() {
         super('auth');
+        this.processJob('addAuthuserToDb', 5, authWorker.addAuthUserToDB);
     }
 
 
