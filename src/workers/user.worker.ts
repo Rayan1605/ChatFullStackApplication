5cfc920a07ef022f764e39f51a9684deaad9b9f6
rayan <rayanzyad99@gmail.com>
2024-03-29 15:17:28 +0000

    Refactoring

--- src/workers/user.worker.ts
+++ src/workers/user.worker.ts
@@ -2,7 +2,7 @@ import {DoneCallback, Job} from 'bull';
 import Logger from 'bunyan';
 import {config} from "@root/config";
 import {Error} from "mongoose";
-import {authService} from "@services/DB/AuthService";
+import {userService} from "@services/DB/UserService";
 
 const log: Logger = config.createLogger('userWorker');
 
@@ -12,7 +12,7 @@ class UserWorker {
             log.info('Adding user to database');
             const value = job.data.value // our data is held in this variable
             // add method to send to database
-            await authService.addAuthUserToDb(value); // this is used to add the user to the database
+            await userService.addUserData(value) // this is used to add the user to the database
             job.progress(100) // this is used to show the progress of the job
 
             done(null, job.data);
@@ -24,4 +24,4 @@ class UserWorker {
 
 }
 
-export const uuthWorker : UserWorker = new UserWorker();
+export const userWorker : UserWorker = new UserWorker();
